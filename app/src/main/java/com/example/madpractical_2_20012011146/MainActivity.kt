9bc7e515878de5afb14665156de9238322d6cc9c
Keyur Raval <ravalkeyur054@gmail.com>
2022-08-27 19:29:37 +0530

    Background colour changed

--- app/src/main/java/com/example/madpractical_2_20012011146/MainActivity.kt
+++ app/src/main/java/com/example/madpractical_2_20012011146/MainActivity.kt
@@ -2,15 +2,16 @@ package com.example.madpractical_2_20012011146
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
-    val TAG="MainActivity"
+    private var TAG="MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        showMessage(R.layout.activity_main)
+        showMessage("onCreate function called")
     }
 
     private fun showMessage(msg:String){
